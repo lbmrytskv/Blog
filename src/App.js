c57d0f8beb0517d1fe034b57e39f33f3e702d66b
Liubomyr Yatskiv <liubomyryatskiv97@gmail.com>
2023-03-29 17:12:20 +0200

    made a data range filter

--- src/App.js
+++ src/App.js
@@ -6,32 +6,51 @@ import Header from "./Header";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-
-
 export default function App() {
-    const blogData = data.map(item => {
-      return (
-        <Blog
-        item = {item}/>
-      )
-    }) 
-
-    
-function chooseCategory() {
-  const technology = data.filter(item=> item.category === "technology")
-  return ( console.log(technology))
  
+  const [category, setCategory] = React.useState();
+
+    const filteredByCategory = category
+        ? data.filter(item => item.category === category)
+        : data;
+
+    const blogData = filteredByCategory.map(item => {
+        return (
+            <Blog
+                item={item}/>
+        )
+    })
+
+    function chooseCategory(event) {
+        const cat = event.target.type
+            setCategory(cat)
+    }
+      
+    const [startDate, setStartDate] = React.useState();
+      const [endDate, setEndDate] = React.useState();
+    
+    function chooseStartDate(event) {
+      const start = event.target.value
+      setStartDate(start)
+    };
+
+    function chooseEndDate(event) {
+      const end = event.target.value
+      setEndDate(end)
+    }
+
+    const filteredByDate = data.filter((item) => {
+      startDate && endDate ? item.date >= startDate && item.date <= endDate : data;
+    }
+    )
+    return (
+        <div>
+            <Header 
+                    chooseCategory={chooseCategory}
+                    chooseStartDate={chooseStartDate}
+                    chooseEndDate={chooseEndDate}
+                    />
+            {blogData}
+        </div>
+    )
 }
-
-
-  return (
-    <div>
-      <Header
-      technology = {chooseCategory}/>
-      {blogData}
-    </div>
-  )
-  
-}
-
-
--- src/Header.js
+++ src/Header.js
@@ -1,22 +1,37 @@
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 
-const headerlist = [{href: "#/action-1",
-title: "technology",
-type: "technology"
-}]
-export default function Header(props) {
-  //const blogData = data.map(item => {
-  return (
-    <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-      (headerlist.map(header =>
-      return (<Dropdown.Item href="#/action-1" 
-      onClick={props.technology}>technology</Dropdown.Item> )))
-    <Dropdown.Item href="#/action-1" onClick={props.technology}>technology</Dropdown.Item>
-    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-  </DropdownButton>
-);
-}
 
+const headerList = [
+  {title: "All"},
+  {title: "Technology",
+  category: "technology"},
+  {title: "Travel",
+  category: "travel"},
+  {title: "Food",
+  category: "food"},]
+
+export default function Header(props) {
  
+  return (
+    <div className='header'>
+      <DropdownButton id="dropdown-basic-button" title="Categories">
+          {headerList.map((header) => (
+              <Dropdown.Item 
+                  onClick={props.chooseCategory}
+                  type={header.category}>
+                  {header.title}
+              </Dropdown.Item>
+          ))
+          }
+      </DropdownButton>
+      <label>Start Date:
+        <input type="date" value={startDate} onChange={props.chooseStartDate}/>
+      </label>
+      <label>
+        End Date: 
+        <input type="date" value={endDate} onChange={props.chooseEndDate}/>
+      </label>
+      </div>
+  );
+}
