491a2b05577cb02251c06c081fab4c14fdb91f37
Liubomyr Yatskiv <liubomyryatskiv97@gmail.com>
2023-03-30 13:11:07 +0200

    added sorting of data in descending order

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Blog from "./Blog";
 import data from "./data.json";
 import  './styles.css';
@@ -7,58 +7,73 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default function App() {
- 
-  const [category, setCategory] = React.useState();
+  const [category, setCategory] = useState();
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
+  const [sortedData, setSortedData] = useState(data);
 
-    const filteredByCategory = category
-        ? data.filter(item => item.category === category)
-        : data;
+  useEffect(() => {
+    const sorted = [...sortedData];
+    sorted.sort((a, b) => new Date(b.date) - new Date(a.date));
+    setSortedData(sorted);
+  }, []);
 
-    const blogData = filteredByCategory.map(item => {
-        return (
-            <Blog
-                item={item}/>
-        )
-    })
+  useEffect(() => {
+    const filtered = data.filter(item => {
+      // apply category filter
+      if (category && item.category !== category) {
+        return false;
+      }
+      // apply date range filter
+      if (startDate && endDate) {
+        return item.date >= startDate && item.date <= endDate;
+      }
+      return true;
+    })}, [category, startDate, endDate]);
 
-    function chooseCategory(event) {
-        const cat = event.target.type
-            setCategory(cat)
-    }
-      
-    const [startDate, setStartDate] = React.useState();
-      const [endDate, setEndDate] = React.useState();
-    
-    function chooseStartDate(event) {
-      const start = event.target.value
-      setStartDate(start)
-    };
-
-    function chooseEndDate(event) {
-      const end = event.target.value
-      setEndDate(end)
-    }
-
-    const filteredByDate = startDate && endDate 
-    ? data.filter(item => item.date >= startDate && item.date <= endDate) : data;
-    
-    const dateData = filteredByDate.map(item =>{
-      return (
-        <Blog
-        item={item}/>
-      )
-    })
-
-    
+  const blogData = sortedData.map(item => {
     return (
-        <div>
-            <Header 
-                    chooseCategory={chooseCategory}
-                    chooseStartDate={chooseStartDate}
-                    chooseEndDate={chooseEndDate}
-                    />
-            {blogData}
-            {dateData}
-        </div>
+      <Blog
+        item={item}/>
     )
+  });
+
+  function chooseCategory(event) {
+    const cat = event.target.type;
+    setCategory(cat);
+  }
+      
+  function chooseStartDate(event) {
+    const start = event.target.value;
+    setStartDate(start);
+  };
+
+  function chooseEndDate(event) {
+    const end = event.target.value;
+    setEndDate(end);
+  }
+
+  function sortData() {
+    const sorted = [...sortedData];
+    if (sortedData.length > 0 && sortedData[0].date > sortedData[sortedData.length - 1].date) {
+      sorted.sort((a, b) => new Date(a.date) - new Date(b.date));
+    } else {
+      sorted.sort((a, b) => new Date(b.date) - new Date(a.date));
+    }
+    setSortedData(sorted);
+  }
+
+  return (
+    <div>
+      <Header 
+        chooseCategory={chooseCategory}
+        chooseStartDate={chooseStartDate}
+        chooseEndDate={chooseEndDate}
+        startDate={startDate}
+        endDate={endDate}
+        sortData={sortData}
+      />
+      {blogData}
+    </div>
+  )
 }
